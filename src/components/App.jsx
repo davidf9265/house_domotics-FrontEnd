8b7ebd29a29df314d242e6185d1c55ccc0ad0904
David Franco Ospina <david.franco@correounivalle.edu.co>
2023-01-01 21:20:32 -0500

    project init

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/global.scss'; 
+
+const App = () => {
+    return (
+        <div>
+            hola mundo
+        </div>
+    );
+};
+
+export default App;
