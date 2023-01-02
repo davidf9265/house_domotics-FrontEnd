55125a3172560712b7cc4ff0bca1429ae2eb346b
David Franco Ospina <david.franco@correounivalle.edu.co>
2023-01-02 01:12:46 -0500

    estilos de dashboard

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -13,20 +13,20 @@ const Dashboard = () => {
 
                 </div>
                 <div className='middle-toolbar'>
-                    <div>
-                        <button></button>
-                        <button></button>
-                        <button></button>
+                    <div className='upper-options'>
+                        <button className='data-stats-button'>DATA / STATS</button>
+                        <button className='dashboard-button'>DASHBOARD</button>
+                        <button className='security-button'>SECURITY</button>
                     </div>
                     <div className='main-visual'></div>
-                    <div>
-                        <button></button>
-                        <button></button>
-                        <button></button>
+                    <div className='lower-options'>
+                        <button className='hydro-system-button'>HYDRO SYSTEM</button>
+                        <button className='users-button'>USERS</button>
+                        <button className='lab-button'>LAB</button>
                     </div>
                 </div>
                 <div className='right-toolbar'>
-                    
+
                 </div>
             </div>
         </div>
