10d15f111ccbe63097c2d76ae7599246cfe6c9b0
David Franco Ospina <david.franco@correounivalle.edu.co>
2023-01-03 18:55:25 -0500

    state issue with button style resolved

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,32 +9,17 @@ class Button extends Component {
             label: null
         };
     }
-    handleClick = event => {
-        console.log(this.state.value)
+    handleClick = () => {
         if(this.state.value != null){
-            console.log("not null")
-            console.log(this.state.value)
             this.setState({value: !this.state.value}) 
-            console.log(this.state.value)
         }else{
-            console.log("null")
             this.setState({value: true})
         }
-        console.log(this.state.value)
-        if(this.state.value){
-            event.currentTarget.classList.add('custom-button-pressed')
-            event.currentTarget.classList.remove('custom-button')
-        }else{
-            event.currentTarget.classList.add('custom-button')
-            event.currentTarget.classList.remove('custom-button-pressed')
-        }
-        // event.currentTarget.classList.remove('custom-button')
     }
-
     render() {
         return (
             <button
-                className="custom-button elevation-5"
+                className={this.state.value ? "custom-button-pressed elevation-5": 'custom-button elevation-5'}
                 onClick={this.handleClick}>
                 {this.props.label}
             </button>
