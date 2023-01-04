d830c11c4db769f63dc72558cf3dfce7da5fb7ca
David Franco Ospina <david.franco@correounivalle.edu.co>
2023-01-04 01:59:29 -0500

    single button active at a time

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,25 +2,26 @@ import React, { Component } from 'react';
 
 class Button extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            value: false,
-            label: null
-        };
-    }
-    handleClick = () => {
-        if(this.state.value != null){
-            this.setState({value: !this.state.value}) 
-        }else{
-            this.setState({value: true})
-        }
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         value: false,
+    //         label: null
+    //     };
+    // }
+    // handleClick = () => {
+    //     if(this.state.value != null){
+    //         this.setState({value: !this.state.value}) 
+    //     }else{
+    //         this.setState({value: true})
+    //     }
+    // }
     render() {
         return (
             <button
-                className={this.state.value ? "custom-button-pressed elevation-5": 'custom-button elevation-5'}
-                onClick={this.handleClick}>
+                className={this.props.isActive ? "custom-button-pressed elevation-5": 'custom-button elevation-5'}
+                // onClick={this.handleClick}
+                onClick={() => this.props.onClick()}>
                 {this.props.label}
             </button>
         );
--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/dashboard.scss'
 import '../styles/house-top-view.scss'
 import '../styles/elevations.scss'
@@ -8,54 +8,81 @@ import CustomDate from '../components/CustomDate';
 import Button from '../components/Button';
 import Zone from '../components/Zone';
 
-const Dashboard = () => {
-    return (
-        <div className='app-grid'>
-            <div className='header-options elevation-20'>
-                <div className="w3-bar-item">user</div>
-                <div className="w3-bar-item">voice assistant: enabled</div>
-                <CustomDate className="w3-bar-item"></CustomDate>
-                <Clock className="w3-bar-item">w3-bar-item</Clock>
-                <img src={gear} alt="settings-icon"/>
-            </div>
-            <div className='dashboard-interactive'>
-                <div className='left-toolbar'>
-
+class Dashboard extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            buttons: Array(6).fill(false)
+        };
+    }
+    handleClick(i){
+        const Buttons = Array(6).fill(false)
+        Buttons[i] = true;
+        this.setState({buttons: Buttons});
+    }; 
+    renderButton(i,label){
+        return (
+            <Button 
+                label={label} 
+                onClick={() => this.handleClick(i)} 
+                isActive={this.state.buttons[i]}/>
+        );
+    }
+    render(){
+        return (
+            <div className='app-grid'>
+                <div className='header-options elevation-20'>
+                    <div className="w3-bar-item">user</div>
+                    <div className="w3-bar-item">voice assistant: enabled</div>
+                    <CustomDate className="w3-bar-item"></CustomDate>
+                    <Clock className="w3-bar-item">w3-bar-item</Clock>
+                    <img src={gear} alt="settings-icon"/>
                 </div>
-                <div className='middle-toolbar'>
-                    <div className='upper-options'>
-                        <Button label="DATA / STATS"></Button>
-                        <Button label="DASHBOARD"></Button>
-                        <Button label="SECURITY"></Button>
+                <div className='dashboard-interactive'>
+                    <div className='left-toolbar'>
+
                     </div>
-                    <div className='main-visual elevation-8'>
-                        <div className='group'>
-                            <Zone className='rectangle_1 elevation-6'></Zone>
-                            <Zone className='rectangle_2 elevation-6'></Zone>
-                            <Zone className='rectangle_3 elevation-6'></Zone>
-
-                            <Zone className='rectangle_4 elevation-6'></Zone>
-                            <Zone className='rectangle_5 elevation-6'></Zone>
-                            <Zone className='rectangle_6 elevation-6'></Zone>
-
-                            <Zone className='rectangle_7 elevation-6'></Zone>
-                            <Zone className='rectangle_8 elevation-6'></Zone>
-                            <Zone className='rectangle_9 elevation-6'></Zone>
+                    <div className='middle-toolbar'>
+                        <div className='upper-options'>
+                            {this.renderButton(0, "DATA / STATS")}
+                            {this.renderButton(1, "DASHBOARD")}
+                            {this.renderButton(2, "SECURITY")}
+                            {/* <Button label="DATA / STATS" onClick=""></Button>
+                            <Button label="DASHBOARD"></Button>
+                            <Button label="SECURITY"></Button> */}
                         </div>
+                        <div className='main-visual elevation-8'>
+                            <div className='group'>
+                                <Zone className='rectangle_1 elevation-6'></Zone>
+                                <Zone className='rectangle_2 elevation-6'></Zone>
+                                <Zone className='rectangle_3 elevation-6'></Zone>
+
+                                <Zone className='rectangle_4 elevation-6'></Zone>
+                                <Zone className='rectangle_5 elevation-6'></Zone>
+                                <Zone className='rectangle_6 elevation-6'></Zone>
+
+                                <Zone className='rectangle_7 elevation-6'></Zone>
+                                <Zone className='rectangle_8 elevation-6'></Zone>
+                                <Zone className='rectangle_9 elevation-6'></Zone>
+                            </div>
+
+                        </div>
+                        <div className='lower-options'>
+                            {/* <Button label="HYDRO SYSTEM"></Button>
+                            <Button label="USERS"></Button>
+                            <Button label="LAB"></Button> */}
+                            {this.renderButton(3, "HYDRO SYSTEM")}
+                            {this.renderButton(4, "USERS")}
+                            {this.renderButton(5, "LAB")}
+                        </div>
+                    </div>
+                    <div className='right-toolbar'>
 
                     </div>
-                    <div className='lower-options'>
-                        <Button label="HYDRO SYSTEM"></Button>
-                        <Button label="USERS"></Button>
-                        <Button label="LAB"></Button>
-                    </div>
-                </div>
-                <div className='right-toolbar'>
-
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default Dashboard;
